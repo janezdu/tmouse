70a460b214e2777f7f75ac729f0e0e28edc01fab
mdb293 <mdb293@cornell.edu>
2017-11-13 02:30:44 -0500

    parameters as good as tthey'll be

--- Makefile
+++ Makefile
@@ -3,11 +3,7 @@ default :
 	PYTHONPATH="." luigi --module "src.pipeline.tasks" RunSimulator --route 10
 local :
 	rm -r tmp
-	PYTHONPATH="." luigi --local-scheduler --module "src.pipeline.tasks" RunSimulator --route 10 --is-diesl
-	PYTHONPATH="." luigi --local-scheduler --module "src.pipeline.tasks" RunSimulator --route 10
-clean:
-    rm -rf ./tmp
-graph:
-	python ./src/plotting/plot_fuel.py
-remake: 
-	clean default
+	PYTHONPATH="." luigi --local-scheduler --module "src.pipeline.tasks" RunSimulator --route 17 --is-diesl
+	PYTHONPATH="." luigi --local-scheduler --module "src.pipeline.tasks" RunSimulator --route 17
+all :
+    PYTHONPATH="." luigi --local-scheduler --module "src.pipeline.tasks" AllReports
--- src/sim/constants.py
+++ src/sim/constants.py
@@ -5,8 +5,8 @@ SIMPLE_DRIVER_DECELERATION = 0.42
 #Engine constants
 #acceleration due to gravity, m/s^2
 g = 9.8
-#mass of bus in kg
-MASS = 1000
+#mass of bus in kg (30000 lbs)
+MASS = 13607.771
 #drag parameters
 #density of air
 ro = 1.225
@@ -15,15 +15,16 @@ A = 9
 #drag coefficient
 Cd = 0.2
 
-DIESEL_ENGINE_EFFICIENCY = 0.5
-ELECTRIC_ENGINE_EFFICIENCY = 0.3
+#Joules/liter
+DIESEL_ENGINE_EFFICIENCY = 1.8/0.0000001
+ELECTRIC_ENGINE_EFFICIENCY = 1
+#power 20kWatts
+POWER_CAP_ELECTRIC = 20000
 #power
-POWER_CAP_ELECTRIC = 2000000000
-#power
-MAX_BATTERY_CHARGE_RATE = 100
-#battery capacity
-BATTERY_CAP = 1000
+MAX_BATTERY_CHARGE_RATE = 20000
+#battery capacity 1.3kWh (kilowatt hours)
+BATTERY_CAP = 4680000
 #battery charging when using diesel
-BATTERY_CHARGE_FROM_DIESEL = 1
+BATTERY_CHARGE_FROM_DIESEL = 1000
 
 
