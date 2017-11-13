34c2e28e2b38aa05fd59d7b3e5271d5ce8cb0537
Jane Du <zd53@cornell.edu>
2017-11-12 21:01:14 -0500

    Some more plotting for lat/lon

--- src/plotting/pointval.py
+++ src/plotting/pointval.py
@@ -7,25 +7,27 @@ def speedPlotter(fo):
     data = json.load(fo)
 
     val = [obj['external_state']['speed'] for obj in data]
+    lat = [obj['external_state']['lat'] for obj in data]
+    lon = [obj['external_state']['lon'] for obj in data]
     x = np.arange(len(val))
 
     fig, ax = plt.subplots()
-    ax.stackplot(x,val)
+    ax.scatter(lat, lon)
     plt.show()
 
 
 
 def heatMapBasic(fo, val):
     '''relies on schema wiht lat, lon, some other value to plot'''
-
     data = json.load(fo)
-
     val = [obj[val] for obj in data]
-
+    lat = [obj['lat'] for obj in data]
+    lon = [obj['lon'] for obj in data]
+    
     x = np.arange(len(val))
 
     fig, ax = plt.subplots()
-    ax.stackplot(x,val)
+    ax.scatter(x,val)
     plt.show()
 
-heatMapStates(open(sys.argv[1]))
+speedPlotter(open(sys.argv[1]))
